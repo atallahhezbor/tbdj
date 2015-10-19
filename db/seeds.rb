e975ad760e5938928722dc3f7c2fac51e77ca3ef
Atallah <atallahhezbor@gmail.com>
2015-10-19 12:56:27 -0400

    adds seeds for default user and vibes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# User.find_by_email("test@tbdj.com") || User.create(email: "test@tbdj.com", password: "password", password_confirmation: "password")
+
+user = User.new 
+user.email = 'test@tbdj.com'
+user.password = 'password'
+user.password_confirmation = 'password'
+user.save!
+
+Vibe.create([{ title: 'Kanye and Chill', user_id: 1}, { title: 'Frat Sounds', user_id: 1}, {title: 'Study Music', user_id: 1}])
